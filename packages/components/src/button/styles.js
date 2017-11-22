570d9f0e4cdbe0245a85c13a57a6464518649c1a
geoff w <infou@phola.com>
2017-11-22 05:21:21 +0000

    config plus

---
+++ packages/components/src/button/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default styled.button`
+font-size: 1em;
+margin: 1em;
+padding: 0.25em 1em;
+border-radius: 3px;
+background: none;
+color: limegreen;
+border: 2px solid green;
+`
